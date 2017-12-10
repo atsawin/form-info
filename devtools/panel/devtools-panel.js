5e0e1a4699d83bda9e47367d68c3245ccb467c18
Atsawin Chaowanakritsanakul <joke@nakhon.net>
2017-12-10 21:41:14 +0700

    Refactor code to remove all warning.

--- devtools/panel/devtools-panel.js
+++ devtools/panel/devtools-panel.js
@@ -6,17 +6,64 @@ function refresh() {
   browser.runtime.sendMessage({
     tabId: browser.devtools.inspectedWindow.tabId
   }).then((response) => {
-    var ret = '<table class="form-info"><thead><tr><th>Label</th><th>Name</th><th>Type</th><th>Checked</th><th>Value</th></tr></thead><tbody>';
+    var tableElm = document.createElement('table');
+    tableElm.className = 'form-info';
+    var tableHeadElm = document.createElement('thead');
+    var tableBodyElm = document.createElement('tbody');
+    var tableRowElm = document.createElement('tr');
+    var tableCell = document.createElement('th');
+    tableCell.textContent = 'Label';
+    tableRowElm.appendChild(tableCell);
+    tableCell = document.createElement('th');
+    tableCell.textContent = 'Name';
+    tableRowElm.appendChild(tableCell);
+    tableCell = document.createElement('th');
+    tableCell.textContent = 'Type';
+    tableRowElm.appendChild(tableCell);
+    tableCell = document.createElement('th');
+    tableCell.textContent = 'Checked';
+    tableRowElm.appendChild(tableCell);
+    tableCell = document.createElement('th');
+    tableCell.textContent = 'Value';
+    tableRowElm.appendChild(tableCell);
+    tableHeadElm.appendChild(tableRowElm);
+    tableElm.appendChild(tableHeadElm);
     response.forEach(function(form) {
-      ret += '<tr class="form"><td colspan="5">' + form.name + ': ' + form.method + ': ' + form.action + '</td></tr>';
+      tableRowElm = document.createElement('tr');
+      tableRowElm.className = 'form';
+      tableCell = document.createElement('td');
+      tableCell.setAttribute('colspan', '5');
+      tableCell.textContent = 'Name: ' + form.name + ', Method: ' + form.method + ', URL: ' + form.action;
+      tableRowElm.appendChild(tableCell);
+      tableBodyElm.appendChild(tableRowElm);
       form.input.forEach(function(item) {
-        ret += '<tr><td>' + item.label + '</td><td>' + item.name + '</td><td>' + item.type + '</td><td>' + (item.checked ? 'Yes' : '') + '</td><td>' + item.value + '</td></tr>';
+        tableRowElm = document.createElement('tr');
+        tableCell = document.createElement('td');
+        tableCell.textContent = item.label;
+        tableRowElm.appendChild(tableCell);
+        tableCell = document.createElement('td');
+        tableCell.textContent = item.name;
+        tableRowElm.appendChild(tableCell);
+        tableCell = document.createElement('td');
+        tableCell.textContent = item.type;
+        tableRowElm.appendChild(tableCell);
+        tableCell = document.createElement('td');
+        tableCell.textContent = item.checked ? 'Yes' : '';
+        tableRowElm.appendChild(tableCell);
+        tableCell = document.createElement('td');
+        tableCell.textContent = item.value;
+        tableRowElm.appendChild(tableCell);
+        tableBodyElm.appendChild(tableRowElm);
       });
     });
-    ret += '</tbody></table>';
-    document.getElementById("result").innerHTML = ret;
+    tableElm.appendChild(tableBodyElm);
+    var resultElm = document.getElementById('result');
+    while (resultElm.firstChild) {
+      resultElm.removeChild(resultElm.firstChild);
+    }
+    resultElm.appendChild(tableElm);
   });
 }
 
-document.getElementById("button_message").addEventListener("click", refresh);
+document.getElementById('button_message').addEventListener('click', refresh);
 browser.devtools.network.onNavigated.addListener(refresh);
